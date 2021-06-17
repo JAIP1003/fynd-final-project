abfb41d2a02caf95b0cee633dfc268c43adb5950
JAIP1003 <jgatep@gmail.com>
2021-06-17 19:57:52 +0530

    gitignore

--- server/app.js
+++ server/app.js
@@ -1,4 +1,22 @@
 const express = require( 'express' );
 const morgan = require( 'morgan' );
 const bodyParser = require( 'body-parser' );
+const { urlencoded } = require('body-parser');
 
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.use( express.json() );
+// app.use( morgan );
+
+// app.use(bodyParser.json());
+app.use( express.urlencoded( { extended: false } ) );
+
+
+app.listen( port , error => {
+    if( error ){
+        console.error( error.message );
+    }
+    console.log( `server is running at ${port} `);
+})
